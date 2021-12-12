54e81cca14c35976fa6237101c0dcd81f6f6c75e
s3d-bird <arslank3377@gmail.com>
2021-12-12 16:46:17 +0000

    Added Player class... The base class for each type of player

---
+++ VP Project/Model/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+
+namespace VP_Project.Model.Player
+{
+    internal abstract class Player
+    {
+        public enum PLAYER_LEVEL { WEAK, MEDIUM, STRONG };
+
+        protected PictureBox playerSprite;
+        protected int SPEED;
+        PLAYER_LEVEL playerLevel;
+
+        public abstract void Shoot(PictureBox parentSprite);
+
+        public void MoveForward()
+        {
+            this.playerSprite.Top -= SPEED;
+        }
+        public void MoveBackward()
+        {
+            this.playerSprite.Top += SPEED;
+        }
+        public void MoveLeft()
+        {
+            this.playerSprite.Left -= SPEED;
+        }
+        public void MoveRight()
+        {
+            this.playerSprite.Left += SPEED;
+        }
+    }
+}
